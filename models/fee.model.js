faf7a07035a2bb1b89240715a26aa43218f2950e
srisarvesh r <r.srisarvesh@gmail.com>
2022-09-09 22:49:04 +0530

    After adding constants file

--- models/fee.model.js
+++ models/fee.model.js
@@ -1,1 +1,14 @@
 const mongoose=require('mongoose');
+
+const fee_schema=new mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+        default:"Fee"
+    },
+    type:{
+        type:String,
+        required:true,
+        enum:[]
+    }
+})
---
+++ utils/constants.js
@@ -0,0 +1,24 @@
+module.exports={
+    FEE_TYPES:{
+        EXAM_FEE:"Exam Fee",
+        APPLICATION_FEE:"Application Fee"
+    },
+    NATIONALITY_TYPE:{
+         INDIAN:"INDIAN",
+         FOREIGN:"FOREIGN",
+         NRI:"NRI",
+         SAARC:"SAARC"
+    },
+    COURSE_TYPE:{
+        ALL_COURSES:"ALL_COURSES​",
+        MEDICAL:"MEDICAL",
+        DENTAL:"Dental",
+        AYURVEDA:"Ayurveda"
+    },
+    ALL_LEVELS:{
+        UG:"UG",
+        PG:"PG",
+        DIPLOMA:"DIPLOMA",
+        PHD:"Ph.D"
+    }
+}
